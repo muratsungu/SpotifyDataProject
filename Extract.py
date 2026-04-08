8483d3981da07041aaccbf5f26677f969171d148
Murat Süngü <58601835+muratsungu@users.noreply.github.com>
2026-04-08 21:39:37 +0300

    first pipe

--- Extract.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-# ==========================================
-# 1. EXTRACT (ÇIKARMA)
-# ==========================================
-print("Veri okunuyor...")
-# JSON dosyasını pandas DataFrame olarak okuyoruz
-file_path = 'Spotify Account Data/StreamingHistory_music_0.json' 
-df = pd.read_json(file_path)
-print(df)
